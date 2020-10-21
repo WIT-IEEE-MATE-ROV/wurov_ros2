9fa1bc7477a9b10bcee28048ff519d4aaf195d70
Brennan <brennanmk2200@gmail.com>
2020-10-20 22:54:18 -0400

    Destroyed Camels. Snakes > Camels.

--- wurovMessages/build/auv/CMakeFiles/auv.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/auv"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/auv.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
